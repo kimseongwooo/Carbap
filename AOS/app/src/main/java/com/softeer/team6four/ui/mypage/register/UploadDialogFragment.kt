e6e97b943519cff8b13e0e82af6fed19f1a181c5
kimseongwoo <tjddn929@naver.com>
2024-03-11 16:30:26 +0900

    :art: Refactor : UploadDialogFragment 데이터 바인딩 적용 - UploadDialogFragment 데이터 바인딩 적용 Related to: #181

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/UploadDialogFragment.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/register/UploadDialogFragment.kt
@@ -2,11 +2,9 @@ package com.softeer.team6four.ui.mypage.register
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import coil.load
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.softeer.team6four.R
 import com.softeer.team6four.databinding.FragmentUploadDialogBinding
@@ -20,6 +18,7 @@ class UploadDialogFragment(private val navigationCallback: () -> Unit) : DialogF
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         _binding = FragmentUploadDialogBinding.inflate(layoutInflater)
+
         val builder = MaterialAlertDialogBuilder(requireContext())
             .setBackground(
                 AppCompatResources.getDrawable(
@@ -27,48 +26,24 @@ class UploadDialogFragment(private val navigationCallback: () -> Unit) : DialogF
                     R.drawable.background_upload_dialog
                 )
             )
-        builder.setView(binding.root)
 
+        builder.setView(binding.root)
         with(binding) {
+            lifecycleOwner = this@UploadDialogFragment
+            viewModel = registerViewModel
             btnUpload.setOnClickListener {
                 registerViewModel.registerCharger()
-                requireDialog().dismiss()
+                dismiss()
                 navigationCallback.invoke()
             }
+            btnCancel.setOnClickListener { dismiss() }
         }
-        chargerInfoBinding()
         return builder.create()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
-    }
-
-    private fun chargerInfoBinding() {
-        with(binding) {
-            Log.d("startDateTime", registerViewModel.startDateTime.value.toString())
-            btnCancel.setOnClickListener { requireDialog().dismiss() }
-            tvChargerNickname.text = registerViewModel.chargerNickname.value
-            tvChargerLocation.text = registerViewModel.addressText.value
-            tvChargerPrice.text = getString(R.string.price_format, registerViewModel.price.value)
-            tvChargerSpeed.text = registerViewModel.speedTypeText.value
-            tvChargerType.text = registerViewModel.chargerTypeText.value
-            tvChargerInstallType.text = registerViewModel.installTypeText.value
-            tvStartTime.text = getString(
-                R.string.time_string_format,
-                String.format("%02d", registerViewModel.startDateTime.value)
-            )
-            tvEndTime.text = getString(
-                R.string.time_string_format,
-                String.format("%02d", registerViewModel.endDateTime.value)
-            )
-            ivChargerUploadImage.load(
-                registerViewModel.imageUrl.value.ifEmpty {
-                    R.drawable.charger_default_image
-                }
-            )
-        }
     }
 
     companion object {
