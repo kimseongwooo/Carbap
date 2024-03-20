a558287311b81416a49f01b1814ae9b4f42ec968
kimseongwoo <tjddn929@naver.com>
2024-03-20 16:28:46 +0900

    :sparkles: Feat : ApproveReservationDetail 클래스 생성 - ApproveReservationDetail 데이터 클래스 생성
    Related to: #181

---
+++ AOS/app/src/main/java/com/softeer/team6four/data/remote/charger/model/ApproveReservationDetail.kt
@@ -0,0 +1,8 @@
+package com.softeer.team6four.data.remote.charger.model
+
+data class ApproveReservationDetail(
+    val guestNickname: String = "",
+    val rentalDate: String = "",
+    val returnTime: String = "",
+    val totalFee: Int = 0
+)
