be478cebbe70d8f0736182cb97e32c28c3754532
kimseongwoo <tjddn929@naver.com>
2024-04-05 16:19:35 +0900

    :sparkles: Feat : ChargerReservation 내역 데이터 바인딩 적용 - ChargerReservation 내역 데이터 바인딩 적용
    Related to: #181

--- AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/adapter/ChargerReservationAdapter.kt
+++ AOS/app/src/main/java/com/softeer/team6four/ui/mypage/charger/adapter/ChargerReservationAdapter.kt
@@ -4,43 +4,40 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.softeer.team6four.data.remote.reservation.model.ChargerReservationInfoModel
+import com.softeer.team6four.data.remote.reservation.model.ReservationDetail
 import com.softeer.team6four.databinding.MyChargerReservationItemBinding
 import com.softeer.team6four.databinding.ProgressBarItemBinding
 
-data class ReservationDetail(
-    val reservationId: Int = 0,
-    val guestNickname: String = "",
-    val rentalDate: String = "",
-    val rentalTime: String = "",
-    val totalFee: Int = 0
-)
 
-class ChargerReservationAdapter(private val navigationCallback: (reservationDetail : ReservationDetail)  -> Unit)
-    : RecyclerView.Adapter<RecyclerView.ViewHolder>()
-{
-    private val VIEW_TYPE_ITEM = 0
-    private val VIEW_TYPE_LOADING = 1
+class ChargerReservationAdapter(private val navigationCallback: (reservationDetail: ReservationDetail) -> Unit) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val chargerReservationInfoList = ArrayList<ChargerReservationInfoModel>()
 
-    inner class ChargerReservationViewHolder(private val binding: MyChargerReservationItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ChargerReservationViewHolder(private val binding: MyChargerReservationItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(detailModel: ChargerReservationInfoModel) {
-            binding.tvReservationApplicantNickname.text = detailModel.guestNickname
-            binding.tvReservationDatetime.text = detailModel.rentalDate + " | " + detailModel.rentalTime
-            binding.tvReservationPrice.text = detailModel.totalFee.toString() + "원"
-
-            binding.root.setOnClickListener {
-                navigationCallback(ReservationDetail(detailModel.reservationId, detailModel.guestNickname, detailModel.rentalDate, detailModel.rentalTime, detailModel.totalFee))
+            with(binding) {
+                model = detailModel
+                root.setOnClickListener {
+                    navigationCallback(
+                        ReservationDetail(
+                            detailModel.reservationId,
+                            detailModel.guestNickname,
+                            detailModel.rentalDate,
+                            detailModel.rentalTime,
+                            detailModel.totalFee
+                        )
+                    )
+                }
             }
         }
     }
 
-    inner class LoadingViewHolder(private val binding: ProgressBarItemBinding) : RecyclerView.ViewHolder(binding.root) {
-
-    }
+    inner class LoadingViewHolder(binding: ProgressBarItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun getItemViewType(position: Int): Int {
-        return when (chargerReservationInfoList[position].reservationId)
-        {
+        return when (chargerReservationInfoList[position].reservationId) {
             0 -> VIEW_TYPE_LOADING
             else -> VIEW_TYPE_ITEM
         }
@@ -53,6 +50,7 @@ class ChargerReservationAdapter(private val navigationCallback: (reservationDeta
                 val binding = MyChargerReservationItemBinding.inflate(layoutInflater, parent, false)
                 ChargerReservationViewHolder(binding)
             }
+
             else -> {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ProgressBarItemBinding.inflate(layoutInflater, parent, false)
@@ -91,5 +89,10 @@ class ChargerReservationAdapter(private val navigationCallback: (reservationDeta
     fun clearChargerReservationList() {
         chargerReservationInfoList.clear()
         notifyDataSetChanged()
+    }
+
+    companion object {
+        private const val VIEW_TYPE_ITEM = 0
+        private const val VIEW_TYPE_LOADING = 1
     }
 }
